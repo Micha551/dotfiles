b0d8966aaf0d59dc3229f6e558c832415277180b
Micha551 <michas.grin@gmail.com>
2025-11-20 14:09:22 +0100

    stuff

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -1,40 +1,24 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    config = function()
-      require("mason").setup()
-    end,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "clangd" },
-        automatic_installation = true,
-      })
-    end,
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      local lspconfig = require("lspconfig")
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
-      lspconfig.lua_ls.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.clangd.setup({
-        capabilities = capabilities,
-        vim.diagnostic.disable(),
-        cmd = {
-          "clangd",
-          "--offset-encoding=utf-16",
-          "--header-insertion=never",
-        },
-      })
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-      vim.keymap.set("n", "<leader>d", vim.lsp.buf.definition, {})
-      vim.keymap.set("n", "<leader>r", vim.lsp.buf.declaration, {})
-    end,
-  },
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			vim.lsp.config("lua_ls", {
+				capabilities = capabilities,
+			})
+			vim.lsp.config("clangd", {
+				capabilities = capabilities,
+				vim.diagnostic.enable(false),
+				cmd = {
+					"clangd",
+					"--offset-encoding=utf-16",
+					"--header-insertion=never",
+				},
+			})
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+			vim.keymap.set("n", "<leader>d", vim.lsp.buf.definition, {})
+			vim.keymap.set("n", "<leader>r", vim.lsp.buf.declaration, {})
+		end,
+	},
 }
